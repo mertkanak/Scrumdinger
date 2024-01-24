b9366bca6a27b4806ac81d7bf2cac81824325463
mertkanak <mertkanak@anadoluhayat.com.tr>
2024-01-24 18:12:01 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by mert Kanak on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
