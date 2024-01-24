e8ec0d5af02311f911cd720282bc10da7f1d4879
mertkanak <mertkanak77@icloud.com>
2024-01-24 18:42:16 +0300

    section3 completed
    Signed-off-by: mertkanak <mertkanak77@icloud.com>

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by mert Kanak on 24.01.2024.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill" )
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "forward.fill")
+                    
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
