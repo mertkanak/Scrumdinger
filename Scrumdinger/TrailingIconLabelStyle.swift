0215dbe6dc7d9e7161d6eec75ab82e7c56942a52
mertkanak <mertkanak77@icloud.com>
2024-01-24 19:43:15 +0300

    cardView ended
    Signed-off-by: mertkanak <mertkanak77@icloud.com>

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by mert Kanak on 24.01.2024.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
