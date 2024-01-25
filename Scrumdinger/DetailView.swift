0009c5fa75ee6dae3ab1e004250ac5938f74d527
mertkanak <mertkanak77@icloud.com>
2024-01-25 21:29:27 +0300

    no message
    Signed-off-by: mertkanak <mertkanak77@icloud.com>

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
-
+    
+    @State private var isPresentingEditView = false
 
     var body: some View {
         List {
@@ -43,14 +44,38 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Edit") {
+                    isPresentingEditView = true
+                }
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-    NavigationStack {
-        DetailView(scrum: DailyScrum.sampleData[0])
+        NavigationView {
+            DetailView(scrum: DailyScrum.sampleData[0])
         }
     }
 }
-
